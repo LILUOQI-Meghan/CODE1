9961505606372f7702a001d8cf5f69d1e08110f3
Meghan <meghan060908@qq.com>
2025-10-27 15:23:04 +0800

    hm

---
+++ 冒牌排序.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//√∞≈›≈≈–Ú
+	int num[] = { 3,2,4,6,5,8,7,1,0 };
+	//≈≈–Ú«∞
+	for (int m = 0; m < 9; m++) {
+		cout << num[m] << " ";
+	}
+	cout << endl;
+	for (int i = 0;i<8; i++) {
+		for (int j = 0; j <8-i; j++) {
+			if (num[j] > num[j + 1]) {
+				int temp = num[j];
+				num[j] = num[j + 1];
+                num[j + 1] = temp;
+            }
+		}
+	}
+	//≈≈–Ú∫Û
+	for (int n = 0; n < 9; n++) {
+		cout << num[n] << " ";
+	}
+	cout << sizeof(int*) << endl;
+	return 0;
+}
